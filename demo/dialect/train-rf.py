3006014e3abb0833bf21c6c3b22ad8bbb74e15d2
UH <554c46@gmail.com>
2022-07-07 09:00:16 +0200

    file renamed, save to subfolder, less labels

--- demo/dialect/train-rf.py
+++ demo/dialect/train-rf.py
@@ -29,7 +29,9 @@ X_test = evf.i2f(*X_test)
 # y_test = trf.fit_transform([[s] for s in y_test])
 # y_train = trf.transform([[s] for s in y_train])
 
-labels = labels.tolist()
+y_train = ['de' if y[:3] == "de-" else y for y in y_train]
+y_test = ['de' if y[:3] == "de-" else y for y in y_test]
+labels = ['de', 'nds', 'nds-NL', 'gsw', 'bar', 'pfl', 'ltz', 'ksh', 'lim']
 y_test = [labels.index(y) for y in y_test]
 y_train = [labels.index(y) for y in y_train]
 
@@ -52,7 +54,7 @@ model.fit(
 )
 
 # save model
-joblib.dump(model, './model.joblib')
+joblib.dump(model, './rf/model.joblib')
 
 
 # metrics
@@ -64,7 +66,7 @@ r2_test = model.score(
     X=np.hstack([X_test, np.random.random((X_test.shape[0], 1))]),
     y=y_test)
 
-with open('metrics.json', 'w') as fp:
+with open('./rf/metrics.json', 'w') as fp:
     json.dump({"acc-train": r2_train, "acc-test": r2_test}, fp)
 
 
@@ -73,4 +75,4 @@ df_fi = pd.DataFrame(
     index=xnames.tolist() + ["RANDOM"], data=model.feature_importances_, columns=["fi"])
 df_fi = df_fi.sort_values(by="fi", ascending=False)
 # cutoff = df_fi.loc["RANDOM"].values[0]
-df_fi.to_csv("fi.csv", index=True)
+df_fi.to_csv("./rf/fi.csv", index=True)
