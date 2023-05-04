8c2076fc1095e505e46cd95b806eb096775c734a
Ulf Hamster <554c46@gmail.com>
2023-05-04 12:39:25 +0200

    optional document-level processing, and speed test

--- demo/speed/run.py
+++ demo/speed/run.py
@@ -28,24 +28,31 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-FILES = glob.glob("data/*.txt")
+# FILES = glob.glob("data/*.txt")
+# for FILE in FILES:
 
-for FILE in FILES:
-    with open(FILE, 'r') as fp:
-        sentences = fp.readlines()
-        sentences = [s.strip() for s in sentences]
-        # sentences = sentences[:10]  # For dev
-        logger.info(f"Batch Size: {len(sentences)}")
-        # compute embeddings
-        (
-            feats1, feats2, feats3, feats4, feats5, feats6, feats7, feats8,
-            feats9, feats12, feats13, feats14, hashes15, hashes16,
-            lemmata17, spans, annotations
-        ) = evf.to_int(sentences, measure_time=True, sbert_masking=True)
-        # check how much tim just trankit needs
-        start = timer()
-        for sent in sentences:
-            snt = model_trankit(sent)
-            # snt = snt.get('sentences')[0]
-        logger.info(f"{timer() - start: .6f} sec. elapsed, Trankit Only")
-    break
+FILE = "data/schmoller_volkswirtschaftslehre_1893.txt"
+with open(FILE, 'r') as fp:
+    sentences = fp.readlines()
+    sentences = [s.strip() for s in sentences]
+    #sentences = sentences[:10]  # For dev
+    logger.info(f"Batch Size: {len(sentences)}")
+
+    # sentence-level processing with trankit
+    (
+        feats1, feats2, feats3, feats4, feats5, feats6, feats7, feats8,
+        feats9, feats12, feats13, feats14, hashes15, hashes16,
+        sentences_parsed, lemmata17, spans, annotations
+    ) = evf.to_int(sentences, measure_time=True, sbert_masking=True, document_level=False)
+
+    # document-level processing with trankit
+    (
+        feats1, feats2, feats3, feats4, feats5, feats6, feats7, feats8,
+        feats9, feats12, feats13, feats14, hashes15, hashes16,
+        sentences_parsed, lemmata17, spans, annotations
+    ) = evf.to_int(sentences, measure_time=True, sbert_masking=True, document_level=True)
+
+    # check how much tim just trankit needs
+    start = timer()
+    snt = model_trankit("\n".join(sentences))
+    logger.info(f"{timer() - start: .6f} sec. elapsed, Trankit Only")
